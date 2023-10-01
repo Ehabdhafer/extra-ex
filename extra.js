c398be8a3f72204dd6109f5021067b7c35c4a068
Ehabdhafer <ehabzaferabed@gmail.com>
2023-10-01 20:33:13 +0300

    add examples

--- extra.js
+++ extra.js
@@ -56,36 +56,108 @@
 
 // Q6
 // var num = prompt("choose number from 1 to 9");
-var num = 5;
-switch (num) {
-  case 1:
-    x = "ONE";
-    break;
-  case 2:
-    x = "TWO";
-    break;
-  case 3:
-    x = "THREE";
-    break;
-  case 4:
-    x = "FOUR";
-    break;
-  case 5:
-    x = "FIVE";
-    break;
-  case 6:
-    x = "SIX";
-    break;
-  case 7:
-    x = "SEVEN";
-    break;
-  case 8:
-    x = "EIGHT";
-    break;
-  case 9:
-    x = "NINE";
-    break;
-  default:
-    x = "PLEASE TRY AGAIN";
-}
-document.write(x);
+// var num = 5;
+// switch (num) {
+//   case 1:
+//     x = "ONE";
+//     break;
+//   case 2:
+//     x = "TWO";
+//     break;
+//   case 3:
+//     x = "THREE";
+//     break;
+//   case 4:
+//     x = "FOUR";
+//     break;
+//   case 5:
+//     x = "FIVE";
+//     break;
+//   case 6:
+//     x = "SIX";
+//     break;
+//   case 7:
+//     x = "SEVEN";
+//     break;
+//   case 8:
+//     x = "EIGHT";
+//     break;
+//   case 9:
+//     x = "NINE";
+//     break;
+//   default:
+//     x = "PLEASE TRY AGAIN";
+// }
+// document.write(x);
+
+// ---------------------------------------------------
+
+// Q7
+// for (let i = 0; i <= 5; i++) {
+//   alert(i);
+// }
+
+// ---------------------------------------------------
+
+// Q8
+// let i = 0;
+// let y = "";
+// while (i <= 5) {
+//   y += i + " ";
+//   i++;
+// }
+// alert(y);
+
+// ---------------------------------------------------
+
+// Q9
+// for (i = 0; i <= 20; i++) {
+//   if (i % 3 == 0) {
+//     console.log(i);
+//   }
+// }
+
+// ---------------------------------------------------
+
+// Q10
+// let number;
+// number = prompt("write a number in the range 0 to 100");
+// while (number > 100) {
+//   alert("number should lower than 100");
+//   number = prompt("write a number in the range 0 to 100");
+// }
+
+// ---------------------------------------------------
+
+// Q11
+// let number = prompt("write a number in the range 0 to 100");
+// while (number > 100) {
+//   alert("number should lower than 100");
+//   number = prompt("write a number in the range 0 to 100");
+// }
+// if (isNaN(number)) {
+//   console.log("Invalid input. Please enter a number.");
+// }
+// console.log(number);
+
+// ---------------------------------------------------
+
+// Q12
+// let num1 = prompt("integer numbers");
+// let num2 = parseInt(num1);
+// let num3 = 0;
+// for (i = 0; i <= num2; i++) {
+//   num3 += i;
+// }
+// console.log(num3);
+
+// ---------------------------------------------------
+
+// Q13
+// let num1 = parseInt(prompt("integer numbers"));
+// let num2 = 0;
+// for (i = 0; i <= num1; i++) {
+//   num2 += i;
+// }
+// let avg = num2 / (num1 + 1);
+// console.log(avg);
